5b41b4857aa7b4af7555ba6fe6029bea6c5c90a7
Shivam Keshri <keshrishivam1997@gmail.com>
2021-11-12 17:12:25 +0530

    done

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int b;
-	cin>>b;
-	cout << b;
+	int bo;
+	cin>>bo;
+	cout << bo;
   cout << "Hello World!";
   return 0;
 }
